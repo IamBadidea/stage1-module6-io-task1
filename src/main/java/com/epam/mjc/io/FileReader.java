6be161b5f951a77a4cb7ee1e476dd0e42dfce79e
Filipp <okruzhko.f@gmail.com>
2023-11-12 19:54:34 +0400

    feat: complete task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,50 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static void main(String[] args) {
+        FileReader fl = new FileReader();
+        System.out.println(fl.getDataFromFile(new File("src\\main\\resources\\Profile.txt")).toString());
     }
+
+    public Profile getDataFromFile(File file) {
+        StringBuilder profileData = new StringBuilder();
+        try {
+            FileInputStream stream = new FileInputStream(file);
+            int i;
+            while ((i = stream.read()) != -1) {
+                profileData.append((char) i);
+            }
+            stream.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String string = profileData.toString();
+        String[] splitStrings = string.split("\r\n");
+        String[] profileFields = new String[splitStrings.length];
+
+        for (int i = 0; i < splitStrings.length; i++) {
+            String[] tempArray = splitStrings[i].split(" ");
+            profileFields[i] = tempArray[1];
+        }
+
+        Profile profile = new Profile(
+                                        profileFields[0],
+                                        Integer.parseInt(profileFields[1]),
+                                        profileFields[2],
+                                        Long.parseLong(profileFields[3])
+        );
+
+        return profile;
+    }
+
 }
